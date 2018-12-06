72c8609952fce19eea4c1caf04206b9d8487e752
Vianney Dubus <vianney.dubus@gmail.com>
2018-12-06 11:31:25 +0100

    Add a @GeneratedValue annotation on the mission ID

--- src/main/java/com/github/vdubus/hibernate/domain/jpa/Mission.java
+++ src/main/java/com/github/vdubus/hibernate/domain/jpa/Mission.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
@@ -25,6 +27,7 @@ public class Mission implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Id
